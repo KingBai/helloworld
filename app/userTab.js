2676854484313ae70dc9e188d8df2128416e46df
KingBai <396009206@qq.com>
2017-06-26 02:15:37 +0800

    修改页面

--- app/userTab.js
+++ app/userTab.js
@@ -10,22 +10,21 @@ import {
     ScrollView,
     TouchableOpacity,
     Image,
-    Text
+    Text,
+    TouchableWithoutFeedback
 } from 'react-native';
 import NavigationManager from './NavigationManager';
 import {HelloInputText} from './helloInputText';
 import PickerWidget from './PickerWidget';
 import Communications from 'react-native-communications';
 
-
-import ScrollableTabView, {DefaultTabBar} from 'react-native-scrollable-tab-view';
-
 export default class UserTab extends Component{
 
     constructor(props){
         super(props)
         this.state={
-            result:['1','2','12','21','14','25','16','27','18','29'],
+            isTab1:true,
+            result:['1','2','12','21','14','25','16','27','18','29']
         }
     }
 
@@ -34,102 +33,141 @@ export default class UserTab extends Component{
     }
 
     render() {
-        let c = this.state.result;
-        let area = ['广东省','北京市'];
-        let picked = ['北京市']
 
-        return <ScrollableTabView
-            renderTabBar={() => <DefaultTabBar />}
-            ref={(tabView) => { this.tabView = tabView; }}
-            tabBarUnderlineStyle={{
-                backgroundColor:'#00aaee',
-                height:2
-            }}
-            tabBarBackgroundColor='#FFFFFF'
-            tabBarActiveTextColor='#00aaee'
-            tabBarInactiveTextColor='#3a3a3a'
-            tabBarTextStyle={{fontSize: 14}}
-            onChangeTab={(obj) => {
+        var tabView;
+        if(this.state.isTab1){
+            tabView=
+                <UserInput handleVal={this.handleVal} />
+        }else {
+            tabView=<UserResult resultValue={this.state.result} />
+        }
 
-            }}
-            >
-            <View tabLabel="测算数据">
-                <ScrollView  ref='scroll' style={{backgroundColor:'#f1f1f1'}}>
-                    <View style={styles.box}>
-                        <PickerWidget titleName='市场区域' type='single' data={area} pick={picked}></PickerWidget>
-                        <HelloInputText flag='true' titleName='双边协商价差' unit='厘/千瓦时'></HelloInputText>
-                        <HelloInputText flag='true' titleName='双边协商月度分解电量' unit='万千瓦时'></HelloInputText>
-                        <HelloInputText flag='true' titleName='月度出清结算价差' unit='厘/千瓦时'></HelloInputText>
-                        <HelloInputText flag='true' titleName='月度竞价成交电量' unit='万千瓦时'></HelloInputText>
-                        <HelloInputText flag='true' titleName='合同总电量' unit='万千瓦时'></HelloInputText>
-                        <HelloInputText flag='true' titleName='实际总用电量' unit='万千瓦时'></HelloInputText>
-                        <HelloInputText flag='true' titleName='综合目录电价' unit='万千瓦时'></HelloInputText>
-                        <HelloInputText flag='true' titleName='允许偏差范围' unit='%'></HelloInputText>
+        return(
+
+        <ScrollView  ref='scroll' style={{backgroundColor:'#f1f1f1'}}>
+            <View style={styles.container}>
+                <TouchableWithoutFeedback onPress={()=>{this.setState({isTab1:true})}}>
+                    <View style={this.state.isTab1?styles.tab1:styles.tab2}>
+                        <Text style={this.state.isTab1?styles.textActive:styles.textNormal}>测算数据</Text>
                     </View>
-                    <View>
-                        <TouchableOpacity onPress={()=>{
-                            NavigationManager.navNavigation.navigate("Second");
-                        }}>
-                            <Text style={styles.texLink}>参考广东电力市场交易基本规则（试行）2017-01-17</Text>
-                        </TouchableOpacity>
+                </TouchableWithoutFeedback>
 
-                        <View style={styles.bottomView}>
-                            <TouchableOpacity onPress={
-                                ()=> {this.tabView.goToPage(1)}
-                            }>
-                                <Text style={styles.button}>重置</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={()=>{
-                                this.setState({
-                                    result:['12','21','12','21','14','25','16','27','18','29'],
-                                    flag:true
-                                })
-                            }}>
-                                <Text style={styles.button}>测算</Text>
-                            </TouchableOpacity>
+                <TouchableWithoutFeedback onPress={()=>{this.setState({isTab1:false})}}>
+                    <View style={this.state.isTab1?styles.tab2:styles.tab1}>
+                        <Text style={this.state.isTab1?styles.textNormal:styles.textActive}>测算结果</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+            </View>
+
+            {tabView}
+
+            <View>
+                <TouchableOpacity onPress={()=>{
+                    NavigationManager.navNavigation.navigate("Second");
+                }}>
+                    <Text style={styles.texLink}>参考广东电力市场交易基本规则（试行）2017-01-17</Text>
+                </TouchableOpacity>
+
+                <View style={this.state.isTab1?styles.bottomView:styles.buttonHide}>
+                    <View>
+
+                        <Text style={styles.button}>重置</Text>
+                    </View>
+                    <TouchableWithoutFeedback onPress={() =>{this.setState({
+                        result:['12','21','12','21','14','25','16','27','18','29'],
+                        isTab1:false
+                    })}}>
+                        <View>
+                            <Text style={styles.button}>测算</Text>
                         </View>
+                    </TouchableWithoutFeedback>
+                </View>
 
-                        <TouchableOpacity onPress={() => this.call() }>
-                            <View style={[styles.bottomView,{marginBottom:30}]}>
-                                <Image style={{width:20,height:20}} source={require('./images/tabs/phone.png')}/>
-                                <Text style={styles.callPhone}>010-82781699</Text>
-                            </View>
-                        </TouchableOpacity>
-
+                <TouchableOpacity onPress={() => this.call() }>
+                    <View style={[styles.bottomView,{marginBottom:30}]}>
+                        <Image style={{width:20,height:20}} source={require('./images/tabs/phone.png')}/>
+                        <Text style={styles.callPhone}>010-82781699</Text>
                     </View>
-                </ScrollView>
+                </TouchableOpacity>
+
             </View>
-            <View tabLabel="测算结果">
-                <ScrollView ref='scroll' style={{backgroundColor:'#f1f1f1'}}>
-                    <View style={styles.box}>
-                        <HelloInputText flag='false' textValue={c[0]} titleName='偏差结算电量' unit='万千瓦时'></HelloInputText>
-                        <HelloInputText flag='false' textValue={c[1]} titleName='长协收益' unit='万元'></HelloInputText>
-                        <HelloInputText flag='false' textValue={c[2]} titleName='月度竞价收益' unit='万元'></HelloInputText>
-                        <HelloInputText flag='false' textValue={c[3]} titleName='偏差结算费用' unit='万元'></HelloInputText>
-                        <HelloInputText flag='false' textValue={c[4]} titleName='市场化前支出' unit='万元'></HelloInputText>
-                        <HelloInputText flag='false' textValue={c[5]} titleName='实际缴纳电费' unit='万元'></HelloInputText>
-                    </View>
-                    <View>
-                        <TouchableOpacity onPress={()=>{
-                            NavigationManager.navNavigation.navigate("Second");
-                        }}>
-                            <Text style={styles.texLink}>参考广东电力市场交易基本规则（试行）2017-01-17</Text>
-                        </TouchableOpacity>
+        </ScrollView>
 
-                        <TouchableOpacity onPress={() => this.call() }>
-                            <View style={[styles.bottomView,{marginBottom:30}]}>
-                                <Image style={{width:20,height:20}} source={require('./images/tabs/phone.png')}/>
-                                <Text style={styles.callPhone}>010-82781699</Text>
-                            </View>
-                        </TouchableOpacity>
+        )
 
-                    </View>
-                </ScrollView>
-            </View>
-        </ScrollableTabView>
+
+
     }
 };
+
+class UserInput  extends Component{
+
+    render() {
+        let area = ['广东省','北京市'];
+        let picked = ['北京市'];
+        return(
+            <View style={styles.box}>
+                <PickerWidget titleName='市场区域' type='single' data={area} pick={picked}></PickerWidget>
+                <HelloInputText flag='true' titleName='双边协商价差' unit='厘/千瓦时'></HelloInputText>
+                <HelloInputText flag='true' titleName='双边协商月度分解电量' unit='万千瓦时'></HelloInputText>
+                <HelloInputText flag='true' titleName='月度出清结算价差' unit='厘/千瓦时'></HelloInputText>
+                <HelloInputText flag='true' titleName='月度竞价成交电量' unit='万千瓦时'></HelloInputText>
+                <HelloInputText flag='true' titleName='合同总电量' unit='万千瓦时'></HelloInputText>
+                <HelloInputText flag='true' titleName='实际总用电量' unit='万千瓦时'></HelloInputText>
+                <HelloInputText flag='true' titleName='综合目录电价' unit='万千瓦时'></HelloInputText>
+                <HelloInputText flag='true' titleName='允许偏差范围' unit='%'></HelloInputText>
+            </View>
+    )}
+}
+
+
+class UserResult extends Component{
+
+    render() {
+        let c = this.props.resultValue;
+        return(
+            <View style={styles.box}>
+                <HelloInputText flag='false' textValue={c[0]} titleName='偏差结算电量' unit='万千瓦时'></HelloInputText>
+                <HelloInputText flag='false' textValue={c[1]} titleName='长协收益' unit='万元'></HelloInputText>
+                <HelloInputText flag='false' textValue={c[2]} titleName='月度竞价收益' unit='万元'></HelloInputText>
+                <HelloInputText flag='false' textValue={c[3]} titleName='偏差结算费用' unit='万元'></HelloInputText>
+                <HelloInputText flag='false' textValue={c[4]} titleName='市场化前支出' unit='万元'></HelloInputText>
+                <HelloInputText flag='false' textValue={c[5]} titleName='实际缴纳电费' unit='万元'></HelloInputText>
+            </View>
+        )}
+}
+
+
 const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        flexDirection: 'row'
+    },
+    tab1:{
+        flex:1,
+        backgroundColor:'#fff',
+        borderBottomColor:'#00aaee',
+        borderBottomWidth:1,
+        height:40
+    },
+    tab2:{
+        flex:1,
+        height:40,
+        backgroundColor:'#fff',
+        borderBottomColor:'#f1f1f1',
+        borderBottomWidth:1,
+    },
+    textActive:{
+        textAlign:'center',
+        lineHeight:28,
+        fontSize:16,
+        color:'#00aaee'
+    },
+    textNormal:{
+        textAlign:'center',
+        lineHeight:28,
+        fontSize:16
+    },
     box:{
         marginTop:10,
         backgroundColor:'#ffffff',
@@ -146,7 +184,7 @@ const styles = StyleSheet.create({
     },
     button:{
         flex:1,
-        width:150,
+        width:140,
         height:40,
         backgroundColor: 'white',
         textAlign:'center',
@@ -163,6 +201,9 @@ const styles = StyleSheet.create({
     callPhone:{
         fontSize:12,
         color:'#00aaee',
+    },
+    buttonHide:{
+        display:'none'
     }
 
 });
